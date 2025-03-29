ff8d891e4bf043dc5cdde50856a406ce0dacd1ea
조용원 <auau3@kakao.com>
2025-03-29 18:54:50 +0900

    added markdown embedding

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -5,13 +5,15 @@ import remarkRehype from "remark-rehype";
 import remarkGfm from "remark-gfm";
 import rehypeStringify from "rehype-stringify";
 import rehypeHighlight from "rehype-highlight";
+import rehypeRaw from "rehype-raw";
 
 export function parseMarkdownToHTML(markdown: string) {
   return unified()
     .use(remarkParse)
     .use(remarkBreaks)
     .use(remarkGfm)
-    .use(remarkRehype)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
     .use(rehypeStringify)
     .use(rehypeHighlight)
     .processSync(markdown)
